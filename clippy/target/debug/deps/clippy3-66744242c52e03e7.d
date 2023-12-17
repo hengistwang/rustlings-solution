c7e63afcb2344cc1e89ff373cc7932d2f80a2bbb
hengist <hengistwang666@gmail.com>
2023-12-17 16:53:47 +0800

    fucked

---
+++ clippy/target/debug/deps/clippy3-66744242c52e03e7.d
@@ -0,0 +1,9 @@
+/home/hengist/Projects/rustlings/exercises/clippy/target/debug/deps/libclippy3-66744242c52e03e7.rmeta: clippy3.rs Cargo.toml
+
+/home/hengist/Projects/rustlings/exercises/clippy/target/debug/deps/clippy3-66744242c52e03e7.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
